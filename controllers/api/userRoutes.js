16af2bb1daed174430531fb6cbf2f374cad493a3
PricedCell <cameronbeattie97@gmail.com>
2024-09-05 21:19:28 +1000

    added create user route

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,6 +1,29 @@
 const router = require('express').Router();
 const { User } = require('../../models');
 
+
+
+router.post('/', async (req, res) => {
+  try {
+    const dbUserData = await User.create({
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password,
+    });
+
+    req.session.save(() => {
+      req.session.loggedIn = true;
+
+      res.status(200).json(dbUserData);
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+
+
 router.post('/login', async (req, res) => {
   try {
     const userData = await User.findOne({ where: { email: req.body.email } });
@@ -25,7 +48,9 @@ router.post('/login', async (req, res) => {
       req.session.user_id = userData.id;
       req.session.logged_in = true;
       
-      res.json({ user: userData, message: 'You are now logged in!' });
+      res
+      .status(200)
+      .json({ user: userData, message: 'You are now logged in!' });
     });
 
   } catch (err) {
