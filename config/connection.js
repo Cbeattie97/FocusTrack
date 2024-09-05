87f735dba6b45cfc313fdccc5b8954f985125c91
PricedCell <cameronbeattie97@gmail.com>
2024-09-05 19:52:39 +1000

    deployment issues fixed

--- config/connection.js
+++ config/connection.js
@@ -5,8 +5,8 @@ const sequelize = process.env.DB_URL
   ? new Sequelize(process.env.DB_URL)
   : new Sequelize(
     process.env.DB_NAME,
-    // process.env.DB_USER,
-    // process.env.DB_PASSWORD,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
     {
       host: process.env.DB_HOST || 'localhost',
       dialect: 'postgres', 
