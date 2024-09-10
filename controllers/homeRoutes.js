d4b99f03b8c326c8721a82ad0dfe862211f00dce
Christopher <94519893+chrispychips12@users.noreply.github.com>
2024-09-10 20:38:23 +1000

    added routes for tasks and draw down tasks (for front end only)
    Started styling on landing page of tasks page

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,14 +1,14 @@
+// TODO: uncomment middleware for authentication
+
 const router = require('express').Router();
-const { User } = require('../models');
-const withAuth = require('../utils/auth');
+const { User, Task } = require('../models'); 
+// const withAuth = require('../utils/auth'); 
 
 // GET the landing page
 router.get('/', async (req, res) => {
     try {
-
-        res.render('login', {
-            // TODO logged_in: req.session.logged_in,
-        });
+        // Redirect to tasks page directly for now, without auth check
+        res.redirect('/tasks');
     } catch (err) {
         res.status(500).json(err);
     }
@@ -16,12 +16,37 @@ router.get('/', async (req, res) => {
 
 // GET the login page
 router.get('/login', (req, res) => {
-    if (req.session.logged_in) {
-        res.redirect('/');
-        return;
-    }
+    // If authentication is enabled, check session status
+    // if (req.session.logged_in) {
+    //     res.redirect('/tasks');
+    //     return;
+    // }
 
     res.render('login');
 });
 
+// GET the main tasks page after login
+router.get('/tasks', /* withAuth, */ async (req, res) => {
+    try {
+        res.render('tasks'); 
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// GET the page to display and manage tasks dynamically
+router.get('/drawDownTasks', /* withAuth, */ async (req, res) => {
+    try {
+        const taskData = await Task.findAll({
+        });
+
+        const tasks = taskData.map(task => task.get({ plain: true }));
+
+        res.render('drawDownTasks', { tasks }); 
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
 module.exports = router;
+
